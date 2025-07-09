5a534bc060fcc888f3776c278b8ee9acd9490ab0
kishoreravi24 <kishore24399@gmail.com>
2025-07-09 06:12:44 +0530

    TwoGrams first push

--- Problems/TwoGrams.cpp
+++ Problems/TwoGrams.cpp
@@ -1,14 +1,23 @@
 #include<bits/stdc++.h>
 int main(){
+    // Input/output redirection
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
     int n,strFreq=0;
     std::cin>>n;
+    if(n<2){
+        std::cout<<"";
+        return 0;
+    }
     std::string curr,res;
     char inputChar,prevChar;
     std::unordered_map<std::string,int> freq;
-    for(int i=0;i<n;i++){
+    std::cin>>prevChar;
+    for(int i=1;i<n;i++){
         std::cin>>inputChar;
 
         curr = "";
